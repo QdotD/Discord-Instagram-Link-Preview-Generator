3024456be593374fa1c0a533ed1458cdd2da6a8d
QdotD <quinton.digital@protonmail.com>
2023-07-22 19:52:42 -0700

    linux server config

--- scrape.js
+++ scrape.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 async function downloadVideo(url) {
     // launch command for linux
     const browser = await puppeteer.launch({
+        headless: false,
         executablePath: '/usr/bin/google-chrome-stable',
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
