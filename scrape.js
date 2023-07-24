77764fd92b8dffd82934905d4d420c3d6bd564f0
QdotD <quinton.digital@protonmail.com>
2023-07-24 16:08:28 -0700

    refactored code to use only network requset URL

--- scrape.js
+++ scrape.js
@@ -4,101 +4,64 @@ const fs = require('fs');
 const Discord = require('discord.js');
 require('dotenv').config();
 
-// vars
 let videoUrl = null;
 
-// Puppeteer
-
 async function downloadVideo(url) {
-    // launch command for linux
     const browser = await puppeteer.launch({
         headless: "new",
         executablePath: '/usr/bin/google-chrome-stable',
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
-    // const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    // Enable request interception
     await page.setRequestInterception(true);
 
-    // Define the pattern to match against request URLs.
     const videoRequestPattern = 'https://scontent';
 
-    // Add request listener
     page.on('request', interceptedRequest => {
-        const url = interceptedRequest.url();
+        const requestUrl = interceptedRequest.url();
 
-        // If the request URL matches the pattern of a video file
-        if (url.startsWith(videoRequestPattern)) {
-            videoUrl = url;
+        console.log(`Request made to: ${requestUrl}`);
+
+        if (requestUrl.startsWith(videoRequestPattern)) {
+            videoUrl = requestUrl;
+            interceptedRequest.abort();
+            console.log(`Downloading video from: ${videoUrl}`);
+            downloadAndUploadVideo(videoUrl);
+        } else {
+            interceptedRequest.continue();
         }
-
-        interceptedRequest.continue();
     });
 
-    try {
-        // Navigate to the page containing the video
-        await Promise.all([
-            page.goto(url, { timeout: 60000 }),
-            page.waitForResponse(response => response.url().startsWith(videoRequestPattern)),
-        ]);
-    } catch (e) {
+    await page.goto(url, { timeout: 60000 }).catch(e => {
         if (e instanceof puppeteer.errors.TimeoutError) {
-            // Handle the timeout how you want (e.g., by skipping this URL, retrying, logging, etc.)
             console.log("Timeout error, skipping this URL...");
         } else {
-            throw e;  // For non-timeout errors, still throw the error so you can see it
+            throw e;
         }
-    }
-
-    // Wait for the video element to be loaded
-    try {
-        // Try to find the video element
-        await page.waitForSelector('video', { timeout: 60000 });
-
-        // Code to process the video goes here...
-    } catch (error) {
-        if (error instanceof puppeteer.errors.TimeoutError) {
-            // If the video element wasn't found within the timeout, send a message
-            console.log('Video element not found');
-            // Or use your bot's messaging function to send a message to the user
-        } else {
-            // If some other error occurred, rethrow it
-            throw error;
-        }
-    }
-
-    // Extract video URL from 'src' attribute
-    // const videoUrl = await page.evaluate(() => {
-    //     const videoElement = document.querySelector('video');
-    //     return videoElement ? videoElement.getAttribute('src') : null;
-    // });
+    });
 
     if (!videoUrl) {
         console.log('No video URL found');
         return;
     }
 
-    console.log(`Downloading video from: ${videoUrl}`);
+    await browser.close();
+}
 
-    // Download the video
+async function downloadAndUploadVideo(videoUrl) {
     const response = await axios({
         url: videoUrl,
         method: 'GET',
         responseType: 'stream'
     });
 
-    // Create a write stream to save the video to a file
     const path = 'video.mp4';
     const writer = fs.createWriteStream(path);
 
-    // Handle errors when creating the write stream
     writer.on('error', (err) => {
         console.error(`Error occurred while writing to the file: ${err}`);
     });
 
-    // Pipe the video stream into the write stream
     response.data.pipe(writer);
 
     await new Promise((resolve, reject) => {
@@ -107,20 +70,13 @@ async function downloadVideo(url) {
     });
 
     console.log('Video downloaded successfully. Now uploading to Discord.');
-
-    await browser.close();
 }
 
-
-// Discord
-
 function extractInstagramUrl(text) {
-    // a basic example that just finds the first URL in the text
     const words = text.split(' ');
     return words.find(word => word.startsWith('https://www.instagram.com/'));
 }
 
-// Initialize the client with your intents
 const GUILDS_AND_GUILD_MESSAGES_INTENTS =
     (1 << 0) +  // GUILDS
     (1 << 9) +   // GUILD_MESSAGES
@@ -134,18 +90,14 @@ client.once('ready', () => {
 
 client.on('messageCreate', async message => {
     console.log(`Received a message from ${message.author.tag}: ${message.content}`);
-    console.log('message: ' + message);
+    console.log('Message: ' + message);
 
-    // Check if the message is not from a bot and contains an Instagram URL
     if (!message.author.bot && message.content.includes('instagram.com')) {
-        // Extract the Instagram URL
         const url = extractInstagramUrl(message.content);
-        console.log(`Extracted URL: ${url}`);
+        console.log(`Extracted URL-: ${url}`);
 
-        // Download the video
         await downloadVideo(url);
 
-        // Then send the video to the same channel the message was in
         if (message.channel.type === 0) {
             try {
                 await message.channel.send({
@@ -174,6 +126,5 @@ client.on('messageCreate', async message => {
         console.log(`Message "${message.content}" does not include an Instagram link`)
     }
 });
-
 
 client.login(process.env.DISCORD_BOT_TOKEN);
