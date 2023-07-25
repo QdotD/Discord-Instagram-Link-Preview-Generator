c6aea2beb014f2b2673d059318361e1eb47e5ec7
QdotD <quinton.digital@protonmail.com>
2023-07-24 17:26:19 -0700

    reverted to "added config for linode linux server"

--- scrape.js
+++ scrape.js
@@ -4,64 +4,47 @@ const fs = require('fs');
 const Discord = require('discord.js');
 require('dotenv').config();
 
-let videoUrl = null;
-
 async function downloadVideo(url) {
+    // launch command for linux
     const browser = await puppeteer.launch({
-        headless: "new",
         executablePath: '/usr/bin/google-chrome-stable',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
+    // const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setRequestInterception(true);
 
-    const videoRequestPattern = 'https://scontent';
-
-    page.on('request', interceptedRequest => {
-        const requestUrl = interceptedRequest.url();
-
-        console.log(`Request made to: ${requestUrl}`);
-
-        if (requestUrl.startsWith(videoRequestPattern)) {
-            videoUrl = requestUrl;
-            interceptedRequest.abort();
-            console.log(`Downloading video from: ${videoUrl}`);
-            downloadAndUploadVideo(videoUrl);
-        } else {
-            interceptedRequest.continue();
-        }
-    });
-
-    await page.goto(url, { timeout: 60000 }).catch(e => {
+    // Navigate to the page containing the video
+    try {
+        await page.goto(url, { timeout: 30000 });
+    } catch (e) {
         if (e instanceof puppeteer.errors.TimeoutError) {
+            // Handle the timeout how you want (e.g., by skipping this URL, retrying, logging, etc.)
             console.log("Timeout error, skipping this URL...");
         } else {
-            throw e;
+            throw e;  // For non-timeout errors, still throw the error so you can see it
         }
-    });
-
-    if (!videoUrl) {
-        console.log('No video URL found');
-        return;
     }
 
-    await browser.close();
-}
+    // Wait for the video element to be loaded
+    await page.waitForSelector('video');
 
-async function downloadAndUploadVideo(videoUrl) {
+    // Extract video URL from 'src' attribute
+    const videoUrl = await page.evaluate(() => {
+        return document.querySelector('video').getAttribute('src');
+    });
+
+    console.log(`Downloading video from: ${videoUrl}`);
+
+    // Download the video
     const response = await axios({
         url: videoUrl,
         method: 'GET',
         responseType: 'stream'
     });
 
-    const path = 'video.mp4';
-    const writer = fs.createWriteStream(path);
+    // Create a write stream to save the video to a file
+    const writer = fs.createWriteStream('video.mp4');
 
-    writer.on('error', (err) => {
-        console.error(`Error occurred while writing to the file: ${err}`);
-    });
-
+    // Pipe the video stream into the write stream
     response.data.pipe(writer);
 
     await new Promise((resolve, reject) => {
@@ -70,14 +53,18 @@ async function downloadAndUploadVideo(videoUrl) {
     });
 
     console.log('Video downloaded successfully. Now uploading to Discord.');
+
+    await browser.close();
 }
 
 function extractInstagramUrl(text) {
+    // a basic example that just finds the first URL in the text
     const words = text.split(' ');
     return words.find(word => word.startsWith('https://www.instagram.com/'));
 }
 
-const GUILDS_AND_GUILD_MESSAGES_INTENTS =
+// Initialize the client with your intents
+const GUILDS_AND_GUILD_MESSAGES_INTENTS = 
     (1 << 0) +  // GUILDS
     (1 << 9) +   // GUILD_MESSAGES
     (1 << 15); // MESSAGE_CONTENT
@@ -90,35 +77,27 @@ client.once('ready', () => {
 
 client.on('messageCreate', async message => {
     console.log(`Received a message from ${message.author.tag}: ${message.content}`);
-    console.log('Message: ' + message);
+    console.log('message: ' + message);
 
+    // Check if the message is not from a bot and contains an Instagram URL
     if (!message.author.bot && message.content.includes('instagram.com')) {
+        // Extract the Instagram URL
         const url = extractInstagramUrl(message.content);
-        console.log(`Extracted URL-: ${url}`);
+        console.log(`Extracted URL: ${url}`);
 
+        // Download the video
         await downloadVideo(url);
 
+        // Then send the video to the same channel the message was in
         if (message.channel.type === 0) {
-            try {
-                await message.channel.send({
-                    content: `Can you believe ${message.author} just tried to make you click a link? Here's the video:`,
-                    files: [{
-                        attachment: 'video.mp4',
-                        name: 'video.mp4'
-                    }]
-                });
-                console.log('Video uploaded successfully to Discord.');
-            } catch (error) {
-                console.error('Error occurred while reading from the file: ' + error);
-            }
-
-            if (fs.existsSync('video.mp4')) {
-                try {
-                    fs.unlinkSync('video.mp4');
-                } catch (error) {
-                    console.error('Error occurred while deleting the file: ' + error);
-                }
-            }
+            await message.channel.send({
+                content: `Can you believe ${message.author} just tried to make you click a link? Here's the video:`,
+                files: [{
+                    attachment: 'video.mp4',
+                    name: 'video.mp4'
+                }]
+            });
+            console.log('Video uploaded successfully to Discord.');
         } else {
             console.error('Target channel is not a text channel.');
         }
@@ -126,5 +105,6 @@ client.on('messageCreate', async message => {
         console.log(`Message "${message.content}" does not include an Instagram link`)
     }
 });
+
 
 client.login(process.env.DISCORD_BOT_TOKEN);
